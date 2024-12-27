169618d7a8491ed85f949ff1b80463275f18c03a
Egor <lomachenkovegor@gmail.com>
2024-12-27 03:53:41 +0300

    upd

--- client/src/main/java/com/example/client/controller/ConfigClientController.java
+++ client/src/main/java/com/example/client/controller/ConfigClientController.java
@@ -1,20 +1,21 @@
 package com.example.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
+@Configuration
 public class ConfigClientController {
 
-    @Value("${URL_A: default value}")
-    private String exampleProperty;
-
-    @Value("${another.value: 0}")
-    private int anotherValue;
+    @Value("${refresh.property: default value}")
+    private String example;
 
     @GetMapping("/config")
     public String getConfig() {
-        return String.format("example.property: %s, another.value: %d", exampleProperty, anotherValue);
+        return String.format("example.property: %s", example);
     }
 }
